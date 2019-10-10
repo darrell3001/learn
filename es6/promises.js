3285190ef5fc0ecf1020b556bde2a169ad6e8705
Darrell Sturdivant <darrell3001@gmail.com>
2019-10-09 21:32:00 -0700

    add 4th example of use of promise

--- es6/promises.js
+++ es6/promises.js
@@ -1,31 +1,40 @@
 const fetch = require("node-fetch");
 
-//Simplified
+//Long form
+function getData1(url) { 
+    fetch(url) 
+        .then ( function funcA(response) { return response.json() } )
+        .then ( function funcB(json)     { console.log(json) } )
+        .catch( function funcC(error)    { console.log('error occured in fetch :' + error)} );
+}
+
+//Simplified using anonymous function names
 function getData2(url) { 
     fetch(url) 
-        .then ( function x(response) { return response.json() } )
-        .then ( function y(json) { console.log(json) } )
-        .catch( function z(error) {console.log('error occured in fetch :' + error)} );
+        .then ( function (response) { return response.json() } )
+        .then ( function (json)     { console.log(json) } )
+        .catch( function (error)    { console.log('error occured in fetch :' + error)} );
 }
 
 // Even more simplified
 function getData3(url) {
     fetch(url)
         .then(  (response) => { return response.json() }  )
-        .then(  (json) => { console.log(json) }  )
-        .catch( (error) => { console.log('an error occured ' + error) } )
+        .then(  (json)     => { console.log(json) }  )
+        .catch( (error)    => { console.log('an error occured ' + error) } )
 }
 
 // Most simple
 function getData4(url) {
     fetch(url)
         .then ( response => response.json() )
-        .then ( json => console.log(json) )
-        .catch( error => console.log("BooHoo - An error occured - "+ error) )
+        .then ( json     => console.log(json) )
+        .catch( error    => console.log("BooHoo - An error occured - "+ error) )
 }
 
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos/1';
 
+getData1(apiUrl);
 getData2(apiUrl);
 getData3(apiUrl);
 getData4(apiUrl);
